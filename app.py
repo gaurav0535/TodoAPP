9e98de87508fcfe8acd6fd475740b04fb925ff77
gaurav0535 <gaurav0535@hotmail.com>
2020-09-28 22:19:55 +0530

    latest

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-from flask import Flask, render_template, request, redirect, url_for,jsonify,abort
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -8,20 +8,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://gauravsrivastava@localhost
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean,nullable=False,default = False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+# db.create_all()
 
-#db.create_all()
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id = todo_id).delete()
+        db.session.commit()
+        print("After comit")
+
+    except:
+        print("inside except")
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -46,6 +59,21 @@ def create_todo():
         return jsonify(body)
 
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        print("inside except")
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
