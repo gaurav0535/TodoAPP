18c4c3c392f34146d1d48294d2ae04a036e6d8ad
gaurav0535 <gaurav0535@hotmail.com>
2020-09-29 02:00:52 +0530

    latest

--- app.py
+++ app.py
@@ -16,16 +16,26 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer,db.ForeignKey(
+        'todolist.id'),nullable = False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+
+class TodoList(db.Model):
+    __tablename__ = 'todolist'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo',backref='list',lazy=True)
+
 
 # db.create_all()
 
 @app.route('/todos/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
     try:
-        Todo.query.filter_by(id = todo_id).delete()
+        Todo.query.filter_by(id=todo_id).delete()
         db.session.commit()
         print("After comit")
 
@@ -34,7 +44,7 @@ def delete_todo(todo_id):
         db.session.rollback()
     finally:
         db.session.close()
-    return jsonify({ 'success': True })
+    return jsonify({'success': True})
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -74,6 +84,14 @@ def set_completed_todo(todo_id):
         db.session.close()
     return redirect(url_for('index'))
 
+
+@app.route('/lists/<lists_id>')
+def get_list_todos(lists_id):
+    return render_template('index.html',
+                           data=Todo.query.filter_by(list_id=lists_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos',lists_id=2))
+
+
